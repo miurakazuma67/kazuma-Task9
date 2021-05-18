13afe919a76d4479c10e2caf38efca456ae25b08
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-05-18 21:49:04 +0900

    途中

---
+++ Task9/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task9
+//
+//  Created by 三浦　一真 on 2021/05/18.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet private weak var prefectureLabel: UILabel!
+    @IBOutlet private weak var changeLabel: UILabel!
+    @IBOutlet private weak var chooseButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Task9/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  Task9
+//
+//  Created by 三浦　一真 on 2021/05/18.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var prefecture: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func prefectureChoose(sender: UIStoryboard) {
+
+    }
+    
+    @IBAction func exitCancel(_ segue: UIStoryboardSegue) {
+    }
+
+
+}
