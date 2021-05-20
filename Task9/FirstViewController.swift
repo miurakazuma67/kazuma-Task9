200fffc5e24bcb02e4dd208dae35ebd6ddc36172
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-05-20 23:08:42 +0900

    フィードバックをもとに修正

--- Task9/FirstViewController.swift
+++ Task9/FirstViewController.swift
@@ -9,42 +9,16 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
-    var prefecture: String = ""
-
-    @IBOutlet private weak var prefectureLabel: UILabel!
     @IBOutlet private weak var changeLabel: UILabel!
-    @IBOutlet private weak var chooseButton: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    @IBAction func backFromTokyo(_ segue: UIStoryboardSegue) {
-    self.prefecture = "東京都"
-    change()
-    }
-    
-    @IBAction func backFromKanagawa(_ segue: UIStoryboardSegue) {
-    self.prefecture = "神奈川県"
-    change()
-    }
-    
-    @IBAction func backFromSaitama(_ segue: UIStoryboardSegue) {
-    self.prefecture = "埼玉県"
-    change()
-    }
-    
-    @IBAction func backFromChiba(_ segue: UIStoryboardSegue) {
-    self.prefecture = "千葉県県"
-    change()
+    @IBAction func exitSelect(_ segue: UIStoryboardSegue) {
+        guard let secondVC = segue.source as? SecondViewController,
+              let selectedPrefectureName = secondVC.selectedPrefectureName else { return }
+        
+        changeLabel.text = selectedPrefectureName
     }
     
     @IBAction func exitCancel(_ segue: UIStoryboardSegue) {
-    }
-    
-    
-    func change() {
-        changeLabel.text = prefecture
     }
 }
 
--- Task9/SecondViewController.swift
+++ Task9/SecondViewController.swift
@@ -9,7 +9,26 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private(set) var selectedPrefectureName: String?
+    
+    @IBAction func tokyo(_ sender: Any) {
+        didSelect(prefectureName: "東京都")
+    }
+    
+    @IBAction func kanagawa(_ sender: Any) {
+        didSelect(prefectureName: "神奈川県")
+    }
+    
+    @IBAction func saitama(_ sender: Any) {
+        didSelect(prefectureName: "埼玉県")
+    }
+    
+    @IBAction func chiba(_ sender: Any) {
+        didSelect(prefectureName: "千葉県")
+    }
+    
+    private func didSelect(prefectureName: String) {
+        selectedPrefectureName = prefectureName
+        performSegue(withIdentifier: "ExitSelect", sender: nil)
     }
 }
